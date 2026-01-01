8c6d94735a0427740ad04c67e04e067af0b3c00e
Richard Gwati <haggiog0844@gmail.com>
2026-01-01 23:35:13 +0200

    Add CORS middleware from go-chi/cors
    - Introduced CORS middleware for handling cross-origin requests.
    - Added LICENSE and README.md files for documentation and licensing.
    - Implemented core functionality in cors.go, including options for allowed origins, methods, headers, and credentials.
    - Added utility functions for header parsing and wildcard matching.
    - Updated modules.txt to include go-chi/cors dependency.

--- rss_aggregator/main.go
+++ rss_aggregator/main.go
@@ -3,19 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, RSS Aggregator!")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT not set in environment")
 	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
 	fmt.Printf("Server will start on port: %s\n", portString)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Server failed to start: ", err)
+	}
 }
