6c65c0b12590c6dfeb4bb38fa9dfce204f65bfbc
Richard Gwati <haggiog0844@gmail.com>
2026-01-01 22:54:17 +0200

    feat!: added Godotenv

--- rss_aggregator/main.go
+++ rss_aggregator/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello, RSS Aggregator!")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not set in environment")
+	}
+	fmt.Printf("Server will start on port: %s\n", portString)
 }
