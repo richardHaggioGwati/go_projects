5cea7b1b46ed30ed65de58e6599304dcb701a591
Richard G <haggiog0844@gmail.com>
2026-06-16 22:13:57 +0200

    feat: method invocations example

---
+++ learning-go/types-methods-interfaces/method-invocations.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%s %s, age %d", p.FirstName, p.LastName, p.Age)
+}
+
+func main() {
+	p := Person{
+		FirstName: "Steven",
+		LastName: "Universe",
+		Age: 13,
+	}
+
+	output := p.String()
+
+	fmt.Println(output)
+}
