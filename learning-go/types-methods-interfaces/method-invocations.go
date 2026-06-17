d08405c50db612b49c76c0f7cd7efb96e6a95c75
Richard G <haggiog0844@gmail.com>
2026-06-17 21:16:26 +0200

    feat: methods for nil instances

--- learning-go/types-methods-interfaces/method-invocations.go
+++ learning-go/types-methods-interfaces/method-invocations.go
@@ -44,6 +44,38 @@ func updateCorrectly(c *Counter) {
 	fmt.Println("Update correctly", c.String())
 }
 
+//----methods for nil instances---------------------------
+
+type IntTree struct {
+	val         int
+	left, right *IntTree
+}
+
+func (it *IntTree) Insert(val int) *IntTree {
+	if it == nil {
+		return &IntTree{val: val}
+	}
+	if val < it.val {
+		it.left = it.left.Insert(val)
+	} else if val > it.val {
+		it.right = it.right.Insert(val)
+	}
+	return it
+}
+
+func (it *IntTree) Contains(val int) bool {
+	switch {
+	case it == nil:
+		return false
+	case val < it.val:
+		return it.left.Contains(val)
+	case val > it.val:
+		return it.right.Contains(val)
+	default:
+		return true
+	}
+}
+
 func main() {
 	p := Person{
 		FirstName: "Steven",
@@ -74,4 +106,15 @@ func main() {
 	fmt.Println("in main", u.String())
 	updateCorrectly(&u)
 	fmt.Println("in main", u.String())
+
+	//_____________________________________
+
+	var it *IntTree
+	it = it.Insert(5)
+	it = it.Insert(3)
+	it = it.Insert(10)
+	it = it.Insert(2)
+	fmt.Println(it.Contains(2))  // true
+	fmt.Println(it.Contains(12)) // false
+	fmt.Println(*it.left) // false
 }
