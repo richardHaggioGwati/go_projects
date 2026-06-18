688e1fd374692c8d2f30f505bde31be69cf02425
Richard G <haggiog0844@gmail.com>
2026-06-18 20:50:04 +0200

    feat: composition and promotion

--- learning-go/types-methods-interfaces/method-invocations.go
+++ learning-go/types-methods-interfaces/method-invocations.go
@@ -25,6 +25,24 @@ const (
 
 )
 
+//---------composition and promotion-------
+type Employee struct {
+	Name string
+	ID string
+}
+
+func (e Employee) Description() string {
+	return fmt.Sprintf("%s (%s)", e.Name, e.ID)
+}
+
+type Manager struct {
+	Employee
+	Reports []Employee
+}
+
+func (m Manager) FindNewEmployees() []Employee // followed by business logic
+
+
 
 //-----------------------------------------
 type Person struct {
