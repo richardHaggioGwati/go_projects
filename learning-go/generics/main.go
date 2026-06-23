bf242a6b5daf38b659701965edd94b7f6106fea1
Richard G <haggiog0844@gmail.com>
2026-06-23 23:39:14 +0200

    feat: generic functions abstract algorithms

--- learning-go/generics/main.go
+++ learning-go/generics/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // Stack LIFO (Last in First Out)
-type Stack[T any] struct {
+type Stack[T comparable] struct {
 	vals []T
 }
 
@@ -21,11 +21,76 @@ func (s *Stack[T]) Pop() (T, bool) {
 	return top, true
 }
 
+func (s *Stack[T]) Contains(val T) bool {
+	for _, v := range s.vals {
+		if v == val {
+			return true
+		}
+	}
+	return false
+}
+
+//-------------------------------------------------
+
+//?? Generic Functions Abstract Algorithms
+
+// Map turn a []T1 to a [T]2 using a mapping function
+// This function has two parameter types T1 and T2
+// This works with slices of any type
+func Map[T1, T2 any](s []T1, f func (T1) T2) []T2 {
+	r := make([]T2, len(s))
+	for i,v := range s {
+		r[i] = f(v)
+	}
+	return r
+}
+
+
+// Filter values from a slice using a filter function
+// It returns only a slice with the elements of s
+// for which f returned true
+func Filter[T any](s []T, f func(T) bool) []T {
+	var r []T
+	for _, v := range s {
+		if f(v) {
+			r = append(r, v)
+		}
+	}
+	return r
+}
+
+// Reduce reduces a []T1 to all single values using a reduction function
+func Reduce[T1, T2 any](s []T1, initializer T2, f func(T2, T1) T2) T2 {
+	r := initializer
+	for _, v := range s {
+		r = f(r, v)
+	}
+	return r
+}
+
 func main() {
 	var intStack Stack[int]
 	intStack.Push(10)
+	// intStack.Push("nope") compiler will catch this error
 	intStack.Push(20)
 	intStack.Push(30)
 	v, ok := intStack.Pop()
 	fmt.Println(v, ok)
+	fmt.Println(intStack.Contains(10))
+
+	words := []string{"One", "Potato", "Two", "Potato"}
+	filtered := Filter(words, func(s string) bool {
+		return s != "Potato"
+	})
+	fmt.Println(filtered)
+
+	lengths := Map(filtered, func(s string) int {
+		return len(s)
+	})
+	fmt.Println(lengths)
+
+	sum := Reduce(lengths, 0, func(acc int, val int) int  {
+		return acc + val
+	})
+	fmt.Println(sum)
 }
