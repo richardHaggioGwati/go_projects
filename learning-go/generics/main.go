40a060149cc95f3a0cb5e9b7dbab6c79d6bd0b04
Richard G <haggiog0844@gmail.com>
2026-06-25 23:40:10 +0200

    feat: more generics

--- learning-go/generics/main.go
+++ learning-go/generics/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Stack LIFO (Last in First Out)
 type Stack[T comparable] struct {
@@ -37,14 +40,13 @@ func (s *Stack[T]) Contains(val T) bool {
 // Map turn a []T1 to a [T]2 using a mapping function
 // This function has two parameter types T1 and T2
 // This works with slices of any type
-func Map[T1, T2 any](s []T1, f func (T1) T2) []T2 {
+func Map[T1, T2 any](s []T1, f func(T1) T2) []T2 {
 	r := make([]T2, len(s))
-	for i,v := range s {
+	for i, v := range s {
 		r[i] = f(v)
 	}
 	return r
 }
-
 
 // Filter values from a slice using a filter function
 // It returns only a slice with the elements of s
@@ -68,6 +70,36 @@ func Reduce[T1, T2 any](s []T1, initializer T2, f func(T2, T1) T2) T2 {
 	return r
 }
 
+//------------------------------------------------------
+
+//?? Type terms to specify operators
+
+type Integer interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+func divAndRemainder[T Integer](num, denom T) (T, T, error) {
+	if denom == 0 {
+		return 0, 0, errors.New("cannot divide by zero")
+	}
+	return num / denom, num % denom, nil
+}
+
+// lists all types that support the ==, !=, <, >, <=, and >= operators
+type Ordered interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+		~float32 | ~float64 |
+		~string
+}
+
+//lists all types that support the ==, !=, <, >, <=, and >= operators
+type PrintableInt interface {
+	~int
+	Sting() string
+}
+
 func main() {
 	var intStack Stack[int]
 	intStack.Push(10)
@@ -89,8 +121,13 @@ func main() {
 	})
 	fmt.Println(lengths)
 
-	sum := Reduce(lengths, 0, func(acc int, val int) int  {
+	sum := Reduce(lengths, 0, func(acc int, val int) int {
 		return acc + val
 	})
 	fmt.Println(sum)
+
+	//-------------------------------------------------
+	var a uint = 18_446_744_073_709_551_615
+	var b uint = 9_223_372_036_854_775_808
+	fmt.Println(divAndRemainder(a, b))
 }
